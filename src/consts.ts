40d193051d54c9e2fc2ecc9b28995bff8528e3cf
lukemagicman <278313063+lukemagicman@users.noreply.github.com>
2026-09-07 14:06:06 +0800

    Redesign LukeMagic homepage

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'LukeMagic';
-export const SITE_DESCRIPTION = 'Luke的个人博客, 记录技术、工程与折腾';
+export const SITE_DESCRIPTION = 'Luke的个人博客, 记录技术折腾、AI 工具、个人思考与互联网探索';
