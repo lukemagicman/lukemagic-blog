300f781cf9ee68172a0cbe0c2f0608cc10ebd0fd
lukemagicman <278313063+lukemagicman@users.noreply.github.com>
2026-09-07 13:50:35 +0800

    Personalize LukeMagic site

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'LukeMagic';
+export const SITE_DESCRIPTION = 'Luke的个人博客, 记录技术、工程与折腾';
